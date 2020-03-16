b11c8a967cac2fdc5378f1a1ce2d76e4ee0c33d9
Felix Schmidt <felix.schmidt@rewe-group.com>
2020-03-16 22:01:20 +0100

    Question request added

--- app.py
+++ app.py
@@ -1,20 +1,34 @@
 import xlingqg
-from flask import Flask
+from flask import Flask, jsonify, request
 import waitress
 import argparse
 
-app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "hi"
+def run(host, port):
+    app = Flask(__name__)
+    qg = xlingqg.CrossLingualQuestionGenerator()
+
+    @app.route("/Question", methods=['POST'])
+    def generate_question():
+        # Roger Federer was born 1986 in Switzerland.
+        request_payload = request.get_json(force=True)
+        source_sentence = request_payload['source']
+        question = qg.generate_cross_lingual_question(source_sentence, [5, 6])
+        answer = qg.generate_translated_answer(source_sentence, [5, 6])
+        response = {"question": question,
+                    "answer": ' '.join(dict['token'] for dict in answer)
+                    }
+        return jsonify(response)
+
+    waitress.serve(app, host=args.host, port=args.port)
+
 
 def main():
     qg = xlingqg.CrossLingualQuestionGenerator()
     question = qg.generate_cross_lingual_question(
         u'Roger Federer was born 1986 in Switzerland.', [5, 6])
     answer = qg.generate_translated_answer(
-        u'Roger Federer was born 1986 in Switzerland.',[5,6])
+        u'Roger Federer was born 1986 in Switzerland.', [5, 6])
 
     print('{} {}'.format(question,
                          ' '.join(dict['token'] for dict in answer)))
@@ -22,7 +36,7 @@ def main():
     question = qg.generate_cross_lingual_question(
         u'Roger Federer was born 1986 in Switzerland.', [0, 1])
     answer = qg.generate_translated_answer(
-        u'Roger Federer was born 1986 in Switzerland.',[0,1])
+        u'Roger Federer was born 1986 in Switzerland.', [0, 1])
 
     print('{} {}'.format(question,
                          ' '.join(dict['token'] for dict in answer)))
@@ -30,22 +44,21 @@ def main():
     question = qg.generate_cross_lingual_question(
         u'Roger Federer was born 1986 in Switzerland.', [4])
     answer = qg.generate_translated_answer(
-        u'Roger Federer was born 1986 in Switzerland.',[4])
+        u'Roger Federer was born 1986 in Switzerland.', [4])
 
     print('{} {}'.format(question,
-                         ' '.join(dict['token'] for dict in answer)))                         
+                         ' '.join(dict['token'] for dict in answer)))
+
 
 def _get_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument("--host", type=str, default="0.0.0.0")
     parser.add_argument("--port", type=int, default="5000")
-    parser.add_argument("--url_root", type=str, default="/questionGeneration")
     return parser
 
+
 if __name__ == '__main__':
-    main()
+    # main()
     parser = _get_parser()
     args = parser.parse_args()
-    print(args.url_root)
-    waitress.serve(app, host=args.host, port=args.port)
-
+    run(host=args.host, port=args.port)
